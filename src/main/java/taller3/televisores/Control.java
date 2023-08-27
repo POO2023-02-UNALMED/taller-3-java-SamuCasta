11a06963396d75fe35f711b7eb0d23d3f0b0661d
Samuel Castaño Mira <scastanomi@unal.edu.co>
2023-08-26 21:04:07 -0500

    Control

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,5 +1,69 @@
 package taller3.televisores;
 
 public class Control {
+	public TV tv;
+	
+	public Control() {
+		
+	}
+	public void enlazar(TV tv1) {
+		this.tv= tv1;
+		tv1.setControl(this);
+	}
+	
+	public void turnOn() {
+        if (tv != null) {
+            tv.turnOn();
+        }
+    }
+
+    public void turnOff() {
+        if (tv != null) {
+            tv.turnOff();
+        }
+    }
+
+    public void canalUp() {
+        if (tv != null) {
+            tv.canalUp();
+        }
+    }
+
+    public void canalDown() {
+        if (tv != null) {
+            tv.canalDown();
+        }
+    }
+
+    public void volumenUp() {
+        if (tv != null) {
+            tv.volumenUp();
+        }
+    }
+
+    public void volumenDown() {
+        if (tv != null) {
+            tv.volumenDown();
+        }
+    }
+
+    public void setCanal(int nuevoCanal) {
+        if (tv != null) {
+            tv.setCanal(nuevoCanal);
+        }
+    }
+
+    public void setVolumen(int nuevoVolumen) {
+        if (tv != null) {
+            tv.setVolumen(nuevoVolumen);
+        }
+    }
+	
+	public TV getTelevisor() {
+		return tv;
+	}
+	public void setTelevisor(TV tv1) {
+		this.tv = tv1;
+	}
 
 }
